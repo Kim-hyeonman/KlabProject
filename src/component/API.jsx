d8927291cb393eb9f9ac273d5900e00e52de54f2
hyeonmanKim <hyeonman1108@naver.com>
2024-06-03 20:32:25 +0900

    baseUrl

--- src/component/API.jsx
+++ src/component/API.jsx
@@ -13,7 +13,7 @@ const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
 const URL = `${PROXY}`;
 // Axios 인스턴스를 생성합니다. App.jsx
 const API = axios.create({
-  baseURL: URL,
+  baseURL: 'http://52.78.72.107:8080/',
   });
   
   // 요청 인터셉터 추가

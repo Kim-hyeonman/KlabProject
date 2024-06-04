2f29d1ad876d7615ed7658f254927025a3667ceb
hyeonmanKim <hyeonman1108@naver.com>
2024-06-04 18:10:14 +0900

    Urlchange

--- src/component/WritingDiary.jsx
+++ src/component/WritingDiary.jsx
@@ -130,9 +130,8 @@ function WritingDiary() {
         try {
             const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
             const URL = `${PROXY}`;
-            console.log(URL);
             // await API.post("/dutch", {
-            await axios.post(URL+"/gpt/diary/dutch", null, { params: params, withCredentials: true }).then(res => {
+            await axios.post(URL+"gpt/diary/dutch", null, { params: params, withCredentials: true }).then(res => {
                 setLoading(false);
                 console.log(res);
                 setDiary(res.data);
